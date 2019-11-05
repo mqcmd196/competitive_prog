8f5000f73046553b7e7363d6cd408ecb9aced9bf
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2019-11-05 11:50:27 +0900

    ABC142D solved

--- AtCoder/ABC/142D.cpp
+++ AtCoder/ABC/142D.cpp
@@ -25,6 +25,43 @@ vector<long> divisors(long n, bool sort_mode = false){
     }
     return result;
 }
+
+vector<long> sieve(long N){
+    vector<long> search_list(N-1);
+    vector<long> prime_list;
+    long prime_number;
+
+    //2 ~ Nの整数を昇順で格納
+    for(long i=0; i<N-1; i++){
+        search_list[i] = i + 2;
+    }
+
+    //sqrt(N)まで実行
+    for(long i=0; i<sqrt(N)+1; i++){
+        //素数リストに探索リストの先頭の数を追加
+        //なぜ! = 1としたかは後述
+        if(search_list[i] != 1){
+            prime_number = search_list[i];
+            prime_list.push_back(prime_number);
+        }        
+        //その倍数を探索リストから篩い落とす
+        for(long j=0; j<search_list.size(); j++){
+            if(search_list[j] % prime_number == 0){
+                //.eraseはO(N)なので1を代入することで代用
+                search_list[j] = 1;
+            }
+        }
+    }
+
+    //探索リストに残った数を素数リストに追加
+    for(long i=0; i<search_list.size(); i++){
+        if(search_list[i] != 1){
+            prime_list.push_back(search_list[i]);
+        }
+    }
+
+    return prime_list;
+}
  
 int main(){
 
@@ -32,12 +69,31 @@ int main(){
     cin >> A;
     cin >> B;
 
+    long prime = 0;
     long g = gcd(A, B);
-    vector<long> cd;
+    vector<long> search_list = divisors(g, true);
+    vector<long> prime_list;
 
-    cd = divisors(g);
+    for(long i=1; i<search_list.size(); i++){
+        if(search_list[i] != 1){
+            prime = search_list[i];
+            prime_list.push_back(prime);
+        }
 
-    
+        for(long j=0; j<search_list.size(); j++){
+            if(search_list[j] % prime == 0){
+                search_list[j] = 1;
+            }
+        }
+    }
+
+    for(long i=0; i<search_list.size(); i++){
+        if(search_list[i] != 1){
+            prime_list.push_back(search_list[i]);
+        }
+    }
+
+    cout << prime_list.size() + 1 << endl;
 
     return 0;
 }
