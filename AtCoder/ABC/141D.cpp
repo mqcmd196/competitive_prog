5350455546c5622b196c1be8b58e4eebff4da033
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2020-01-29 16:54:47 +0900

    adios

---
+++ AtCoder/ABC/141D.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int N, M;
+    cin >> N;
+    cin >> M;
+
+    vector<long> A(N);
+    for(int i=0; i<N; i++){
+        cin >> A[i];
+    }
+
+    long max_value;
+
+    //方針:とにかくでかいものを2で割っていく．それの繰り返し
+    //ソートした配列を新しく作るべきか？
+    for(int i=0; i<N; i++){
+        
+    }
+
+    return 0;
+}
