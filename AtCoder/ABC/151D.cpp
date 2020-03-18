916a7772f244e878d690bdd2504bc97a25906bbd
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2020-03-18 11:44:30 +0900

    151D in progress

--- AtCoder/ABC/151D.cpp
+++ AtCoder/ABC/151D.cpp
@@ -13,6 +13,8 @@ int main(){
         }
     }
 
+    int dfs = 
+    
 
 
     return 0;
---
+++ algorithms/graph.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using Graph vector<vector<long>>;
+
+void bfs(Graph G, long start){
+    long v = start;
+
+    vector<bool> seen(Graph.size());
+    seen[v] = true;
+}
