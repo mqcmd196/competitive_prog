035face4275fed3349ab6f3b45851cf0c44d6d63
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2020-09-05 17:11:52 +0900

    36D 1A in progress

---
+++ AtCoder/ABC/36D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+
+    int N;
+    vector<int> a, b;
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> a.at(i);
+        cin >> b.at(i);
+    }
+
+    return 0;
+}
