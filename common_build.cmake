079d76ecea12582480ebd2084ae07db18a948d96
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2024-02-27 17:28:58 +0900

    add CONFIGURE_DEPENDS in GLOB

--- common_build.cmake
+++ common_build.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set(CMAKE_BUILD_TYPE Debug)
 
-file(GLOB cpp_files "*.cpp")
+file(GLOB cpp_files CONFIGURE_DEPENDS "*.cpp")
 
 foreach(file ${cpp_files})
     get_filename_component(name ${file} NAME_WE)
